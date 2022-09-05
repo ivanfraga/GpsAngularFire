3061ac9f3e34a4df541cf56fe3695304f824015f
ivan fraga <edwin.fraga@epn.edu.ec>
2022-09-04 19:08:44 -0500

    comentarios

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCSYu3KbAkn06LL_cxZsJQr1srvHcXKsHQ",
+    authDomain: "appweb-bcd38.firebaseapp.com",
+    databaseURL: "https://appweb-bcd38-default-rtdb.firebaseio.com",
+    projectId: "appweb-bcd38",
+    storageBucket: "appweb-bcd38.appspot.com",
+    messagingSenderId: "267983699764",
+    appId: "1:267983699764:web:4f788a2ac6374da1086f61"
+  }
 };
